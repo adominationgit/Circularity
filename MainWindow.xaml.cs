12097776726ff1066617bfb42894a6e99d7ca74e
domrs <domrs@DOMWINDOWS>
2022-03-06 13:17:19 -0600

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Circularity
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void GetStartedClick(object sender, RoutedEventArgs e)
+        {
+            Window1 TaskList = new Window1();
+            TaskList.Show();
+            this.Close();
+        }
+    }
+}
